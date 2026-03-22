73f61f8acd60f87e07aa3f6f88ce2a4f35ca9fc4
bgarciarodriguez3 <bgarciarodriguez3@gmail.com>
2026-03-22 19:01:27 +0100

    Update premium-products.js

--- config/premium-products.js
+++ config/premium-products.js
@@ -1,1 +1,21 @@
+const PREMIUM_PRODUCTS = {
+  "10496141754705": {
+    name: "Tu Camino, Tu Destino y Tus Decisiones – Mentoría",
+    type: "claridad",
+    formUrl: "https://forms.gle/9m6P5m3pBZ4BEybf9"
+  },
+  "10523108966737": {
+    name: "Claridad en tus Relaciones y tu Camino Sentimental",
+    type: "amor",
+    formUrl: "https://forms.gle/z7Yqenb3VsrAVjij9"
+  },
+  "10667662606673": {
+    name: "Nuevos Comienzos, Liderazgo y Economía Personal",
+    type: "proposito",
+    formUrl: "https://forms.gle/AyAm7JACnZCoXNsy7"
+  }
+}
 
+module.exports = {
+  PREMIUM_PRODUCTS
+}
